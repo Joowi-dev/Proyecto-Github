955ef62a577e92f604ad9f0b7f1d4dd2a76bd5b5
Joowi-dev <mendozaacostajoel@gmail.com>
2026-03-10 13:27:19 +0000

    mensaje con los cambios

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import net.salesianos.persona.Persona;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        Persona Joel;
     }
 }
---
+++ src/net/salesianos/persona/Persona.java
@@ -0,0 +1,9 @@
+package  net.salesianos.persona;
+
+public class Persona {
+    
+    private String nombre;
+    private int dni;
+
+   public Persona
+}
